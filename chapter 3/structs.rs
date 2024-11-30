c317bd99bb5ca250c00f75e3e693d7c87998f42b
whitewizardd <leumasre@gmail.com>
2024-11-30 05:31:40 +0100

    still on struct

--- chapter 3/structs.rs
+++ chapter 3/structs.rs
@@ -3,26 +3,36 @@
 // a struct is a collection or grouping of types
 // with struct we can group associated data types together 
 
-fn main() {
+// fn main() {
 
-    // this below are attributes we need for door which are wriiten in different variables
-    let door_width : u32 = 100;
-    let door_height : u32 = 120;
-    let is_door_open : bool = false;
+//     // this below are attributes we need for door which are wriiten in different variables
+//     let door_width : u32 = 100;
+//     let door_height : u32 = 120;
+//     let is_door_open : bool = false;
 
 
-    // but with struct we can group them all together as they are all associated to that of a door 
+//     // but with struct we can group them all together as they are all associated to that of a door 
 
-    let living_room_door : Door = Door {
-        width: 100,
-        height: 120,
-        is_open: false
-    };
+//     let living_room_door : Door = Door {
+//         width: 100,
+//         height: 120,
+//         is_open: false
+//     };
 
-}
+// }
 
 struct Door {
     width : u32,
     height : u32,
     is_open : bool
+}
+
+impl Door {
+    fn create_new_door(width: u32 , height: u32 , is_open: bool ) -> Self {
+        Door {
+            width,
+            height, 
+            height
+        }
+    }
 }
