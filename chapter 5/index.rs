d9aba1adfd121822406c9ad94582fcd002db065e
whitewizardd <leumasre@gmail.com>
2024-12-06 02:58:03 +0100

    ownership

---
+++ chapter 5/index.rs
@@ -0,0 +1,8 @@
+// TODO: 
+// In this chapter, we will be covering the following topics:
+// Taking ownership of memory
+// Working with both kinds of memory
+// Borrowing memory
+// Working in scopes
+// Controlling mutability
+// Introducing clones
