1ffd72150f71ae35395829644c1497aa3cf0d910
whitewizardd <leumasre@gmail.com>
2024-12-11 23:56:18 +0100

    reassigning values

--- chapter 5/shadowing.rs
+++ chapter 5/shadowing.rs
@@ -1,9 +1,22 @@
 
+// to achieve mutability, we can use shadowing .
+// shadowing is creating another variable that was once declared.
+// fn main() {
 
+//     let x = 20;
+//     let x = 50;
+
+//     assert_eq!(x , 50);
+// }
+
+
+//by reassigning the value to another variable;
 fn main() {
 
-    let x = 20;
-    let x = 50;
+    let x = String::from("rust");
 
-    assert!(x == 20);
+    let z = x + "acean";
+
+    // assert_eq!(z , "rustacean");
+    println!("{}", z);
 }
