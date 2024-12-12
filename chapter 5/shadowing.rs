4aca39b1811f856c3d86faf98abcd94c207f4b75
whitewizardd <leumasre@gmail.com>
2024-12-12 01:23:39 +0100

    mm.

--- chapter 5/shadowing.rs
+++ chapter 5/shadowing.rs
@@ -11,12 +11,57 @@
 
 
 //by reassigning the value to another variable;
+// fn main() {
+
+//     let x = String::from("rust");
+
+//     let z = x + "acean";
+
+//     // assert_eq!(z , "rustacean");
+//     println!("{}", z);
+// }
+
+// we can also achieve mutablility by declaring the variable as mutable using the keyword mut
+
+// fn main() {
+//     let mut x = Vec::<String>::new();
+
+//     x.push(String::from("Hello"));
+//     x.push(String::from("World"));
+//     x.push(String::from("rustacean"));
+
+//     println!("result of the vector is ::: {:?}", x)
+
+// }
+
+
+// mutablility can also be achieved by using functions and for structs as well
+
+fn join_together(target: &mut String, separator: char , list: &[&str]) {
+
+    // this bool variable below allow us to control the dynamics of the instruction
+    let mut is_first : bool = true;
+
+    for element in list.iter() {
+        // the if statement checks if the is_first variable is not true , if it isn't then it adds the separtor to the target as a demacator
+        if !is_first {
+            target.push(separator);
+        }
+        else {
+            is_first = false;
+        }
+
+        target.push_str(element);
+    }
+}
+
 fn main() {
 
-    let x = String::from("rust");
+    let list = ["goat", "sheep", "cow"];
+    let separator : char = '-';
+    let mut target = String::new();
 
-    let z = x + "acean";
+    join_together(&mut target, separator, &list);
 
-    // assert_eq!(z , "rustacean");
-    println!("{}", z);
+    println!("target values are ::: {}", target)
 }
